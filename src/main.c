043b7ea6a43d67a53cc680f9b56423c69959d9c0
Anthonytla <66301752+Anthonytla@users.noreply.github.com>
2021-06-09 07:44:20 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -6,10 +6,7 @@ int main(void) {
     while (!game->quit) {
         game_draw(game);
         game_event(game);
-<<<<<<< HEAD
-=======
         game_move(game);
->>>>>>> 5b00709a96ee74fe022af6685b8cddf3b75fafe8
     }
     
     game_destroy(game);
