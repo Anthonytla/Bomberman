35a4a001531283256f7c8e274052f21d89da61a5
Thua Du Anthony La <la_t@etna-alternance.net>
2021-06-17 13:56:56 -0400

    add server and client

---
+++ client.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/un.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+void client(char *argv, char *port) {
+    struct addrinfo hints;
+    struct addrinfo *res = NULL;
+    struct sockaddr_storage their_addr;
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+
+    int status = getaddrinfo(argv, port, &hints, &res);
+    if (status != 0)
+        printf("%s", gai_strerror(status));
+    
+    int sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+
+    int new_fd = connect(sockfd, res->ai_addr, res->ai_addrlen);
+    if (new_fd < 0)
+        printf("Error connect");
+    char message[128];
+    while (1)
+    {
+        memset(message, '\0', 128);
+        fgets(message, 128, stdin);
+        if (write(sockfd, message, strlen(message)) < 0) {
+            printf("send failed");
+            close(sockfd);
+            return;
+        }
+        printf("sended %s", message);
+        read(sockfd, message, 3);
+        printf("%s\n", message);
+        memset(message, '\0', 128);
+    }
+    close(sockfd);
+    return;
+}
+
+int main(int argc, char **argv)
+{
+    if (argc < 2)
+        return 2;
+    client(argv[1], argv[2]);
+}
--- server.c
+++ server.c
@@ -2,12 +2,15 @@
 #include <string.h>
 #include <sys/un.h>
 #include <sys/types.h>
+#include <sys/time.h>
 #include <sys/socket.h>
 #include <netdb.h>
+#include <unistd.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
 
-void server(char *argv, char *port) {
+void server(char *argv, char *port)
+{
     struct addrinfo hints;
     struct addrinfo *res = NULL;
     struct sockaddr_storage their_addr;
@@ -15,21 +18,44 @@ void server(char *argv, char *port) {
     hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
 
-    int status = getaddrinfo(argv, port, &hints, &res);
-    if (status != 0)
-        printf("%s", gai_strerror(status));
-    
-    int sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+    char *ports[4] = {"8000", "8001", "8002", "8003"};
+    fd_set setFd;
+    FD_ZERO(&setFd);
+    for (int i = 0; i < 4; i++)
+    {
+
+        int status = getaddrinfo(argv, ports[i], &hints, &res);
+        if (status != 0)
+            printf("%s", gai_strerror(status));
+        int sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+
+    }
+
     int bindStatus = bind(sockfd, res->ai_addr, res->ai_addrlen);
     if (bind != 0)
         gai_strerror(bindStatus);
     int listenStatus = listen(sockfd, 10);
-    if (listen != 0)
+    if (listenStatus != 0)
         gai_strerror(listenStatus);
     int addr_size = sizeof their_addr;
-    int new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &addr_size);
-    if (new_fd == -1)
-        gai_strerror(new_fd);
+
+    char buff[10];
+    int n;
+    printf("Waiting for client...\n");
+    while (1)
+    {
+        int new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &addr_size);
+        if (new_fd == -1)
+            gai_strerror(new_fd);
+        printf("New client\n");
+        while ((n = read(new_fd, buff, 10)) > 0)
+        {
+            printf("received %s", buff);
+            write(new_fd, "OK", 3);
+            memset(buff, '\0', 10);
+        }
+    }
+    close(sockfd);
 }
 
 int main(int argc, char **argv)
